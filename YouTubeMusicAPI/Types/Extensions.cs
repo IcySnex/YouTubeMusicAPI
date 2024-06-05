9c2e80fff1974a7e4e58bfc4988c1649017b8873
IcySnex <81762750+IcySnex@users.noreply.github.com>
2024-06-05 19:30:02 +0200

    Added types

---
+++ YouTubeMusicAPI/Types/Extensions.cs
@@ -0,0 +1,47 @@
+﻿namespace YouTubeMusicAPI.Types;
+
+/// <summary>
+/// Contains extension methods for types
+/// </summary>
+public static class Extensions
+{
+    /// <summary>
+    /// Converts search kind to YouTube Music request payload params
+    /// </summary>
+    /// <param name="kind">The shelf kind to convert</param>
+    /// <returns>A YouTube Music request payload params</returns>
+    public static string? ToParams(
+        this ShelfKind? kind) =>
+        kind switch
+        {
+            ShelfKind.Songs => "EgWKAQIIAWoQEAMQChAJEAQQBRAREBAQFQ%3D%3D",
+            ShelfKind.Videos => "EgWKAQIQAWoQEAMQChAJEAQQBRAREBAQFQ%3D%3D",
+            ShelfKind.Albums => "EgWKAQIYAWoQEAMQChAJEAQQBRAREBAQFQ%3D%3D",
+            ShelfKind.CommunityPlaylists => "EgeKAQQoAEABahAQAxAKEAkQBBAFEBEQEBAV",
+            ShelfKind.Artists => "EgWKAQIgAWoQEAMQChAJEAQQBRAREBAQFQ%3D%3D",
+            ShelfKind.Podcasts => "EgWKAQJQAWoQEAMQChAJEAQQBRAREBAQFQ%3D%3D",
+            ShelfKind.Episodes => "EgWKAQJIAWoQEAMQChAJEAQQBRAREBAQFQ%3D%3D",
+            ShelfKind.Profiles => "EgWKAQJYAWoQEAMQChAJEAQQBRAREBAQFQ%3D%3D",
+            _ => null
+        };
+
+    /// <summary>
+    /// Converts string to shelf kind
+    /// </summary>
+    /// <param name="shelfKindString">The string to convert</param>
+    /// <returns>A ShelfKind enum value</returns>
+    public static ShelfKind ToShelfKind(
+        this string? shelfKindString) =>
+        shelfKindString switch
+        {
+            "Songs" => ShelfKind.Songs,
+            "Videos" => ShelfKind.Videos,
+            "Albums" => ShelfKind.Albums,
+            "Community playlists" => ShelfKind.CommunityPlaylists,
+            "Artists" => ShelfKind.Artists,
+            "Podcasts" => ShelfKind.Podcasts,
+            "Episodes" => ShelfKind.Episodes,
+            "Profiles" => ShelfKind.Profiles,
+            _ => ShelfKind.Unknown
+        };
+}
---
+++ YouTubeMusicAPI/Types/ShelfKind.cs
@@ -0,0 +1,14 @@
+﻿namespace YouTubeMusicAPI.Types;
+
+public enum ShelfKind
+{
+    Unknown,
+    Songs,
+    Videos,
+    Albums,
+    CommunityPlaylists,
+    Artists,
+    Podcasts,
+    Episodes,
+    Profiles
+}

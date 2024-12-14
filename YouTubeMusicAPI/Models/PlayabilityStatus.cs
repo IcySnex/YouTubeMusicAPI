32709aa1dce118539302b9c67379380fd0f9979f
IcySnex <lao43919@gmail.com>
2024-12-14 22:51:38 +0100

    Implemented "PlayabilityStatus" for SongVideoInfo

---
+++ YouTubeMusicAPI/Models/PlayabilityStatus.cs
@@ -0,0 +1,31 @@
+﻿namespace YouTubeMusicAPI.Models;
+
+/// <summary>
+/// Contains information about a YouTube Music song/video playability status
+/// </summary>
+public class PlayabilityStatus
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="isOkay">Whether the playability status is okay</param>
+    /// <param name="reasonForNotOkay">The reason why the playability status is not okay (null if 'IsOkay' is true)</param>
+    public PlayabilityStatus(
+        bool isOkay,
+        string? reasonForNotOkay)
+    {
+        IsOkay = isOkay;
+        ReasonForNotOkay = reasonForNotOkay;
+    }
+
+    /// <summary>
+    /// Whether the playability status is okay
+    /// </summary>
+    public bool IsOkay { get; }
+
+    /// <summary>
+    /// The reason why the playability status is not okay
+    /// (null if 'IsOkay' is true)
+    /// </summary>
+    public string? ReasonForNotOkay { get; }
+}

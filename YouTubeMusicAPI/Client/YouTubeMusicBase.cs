386649cd96120227937ce0ee380b377193b1b4db
IcySnex <lao43919@gmail.com>
2024-12-14 22:54:23 +0100

    Updated payload creation

--- YouTubeMusicAPI/Client/YouTubeMusicBase.cs
+++ YouTubeMusicAPI/Client/YouTubeMusicBase.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Specialized;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Web;
 using YouTubeMusicAPI.Internal;
 
@@ -11,33 +15,6 @@ namespace YouTubeMusicAPI.Client;
 /// </summary>
 public class YouTubeMusicBase
 {
-    /// <summary>
-    /// Creates a new YouTube Music request payload
-    /// </summary>
-    /// <param name="hostLanguage">The language for the payload</param>
-    /// <param name="geographicalLocation">The region for the payload</param>
-    /// <returns>The validated HTTP response data</returns>
-    public static Dictionary<string, object> CreatePayload(
-        string hostLanguage = "en",
-        string geographicalLocation = "US")
-    {
-        Dictionary<string, object> payload = new()
-        {
-            ["context"] = new
-            {
-                client = new
-                {
-                    clientName = "WEB_REMIX",
-                    clientVersion = "1.20240918.01.00",
-                    hl = hostLanguage,
-                    gl = geographicalLocation,
-                }
-            }
-        };
-        return payload;
-    }
-
-
     readonly ILogger? logger;
     readonly RequestHelper requestHelper = new();
 
@@ -72,7 +49,7 @@ public class YouTubeMusicBase
         string geographicalLocation = "US",
         CancellationToken cancellationToken = default)
     {
-        string url = Endpoints.Cookies + Endpoints.Save;
+        string url = Endpoints.CookiesUrl + Endpoints.Save;
 
         // Create parameters
         logger?.LogInformation($"[YouTubeMusicBase-GenerateCookiesAsync] Creating parameters.");
@@ -105,10 +82,10 @@ public class YouTubeMusicBase
         (string key, string? value)[] parameterItems,
         CancellationToken cancellationToken = default)
     {
-        string url = Endpoints.WebUrl + apiEndpoint;
+        string url = Endpoints.MusicWebUrl + apiEndpoint;
 
         // Cookies
-        if (requestHelper.Cookies.GetCookies(new(Endpoints.Cookies)).Count < 1)
+        if (requestHelper.Cookies.GetCookies(new(Endpoints.CookiesUrl)).Count < 1)
             await ConsentCookiesAsync();
 
         // Create parameters
@@ -128,9 +105,7 @@ public class YouTubeMusicBase
     /// Sends a new request to the YouTube Music API with a payload
     /// </summary>
     /// <param name="apiEndpoint">The specific API endpoint for the request</param>
-    /// <param name="payloadItems">The items to add to the request payload</param>
-    /// <param name="hostLanguage">The language for the payload</param>
-    /// <param name="geographicalLocation">The region for the payload</param>
+    /// <param name="payload">The request payload</param>
     /// <param name="cancellationToken">The cancellation token to cancel the action</param>
     /// <exception cref="NotSupportedException">May occurs when the json serialization fails</exception>
     /// <exception cref="InvalidOperationException">May occurs when sending the web request fails</exception>
@@ -139,19 +114,10 @@ public class YouTubeMusicBase
     /// <returns>The json object containing the response data</returns>
     public async Task<JObject> SendRequestAsync(
         string apiEndpoint,
-        (string key, object? value)[] payloadItems,
-        string hostLanguage = "en",
-        string geographicalLocation = "US",
+        Dictionary<string, object> payload,
         CancellationToken cancellationToken = default)
     {
-        string url = Endpoints.BaseUrl + apiEndpoint;
-
-        // Create payload
-        logger?.LogInformation($"[YouTubeMusicBase-SendRequestAsync] Creating payload.");
-        Dictionary<string, object> payload = CreatePayload(hostLanguage, geographicalLocation);
-        foreach ((string key, object? value) in payloadItems)
-            if (value is not null)
-                payload[key] = value;
+        string url = Endpoints.MusicApiUrl + apiEndpoint;
 
         // Send request
         string response = await requestHelper.PostAndValidateAsync(url, payload, null, cancellationToken);
@@ -160,4 +126,93 @@ public class YouTubeMusicBase
         logger?.LogInformation($"[YouTubeMusicBase-SendRequestAsync] Parsing response.");
         return JObject.Parse(response);
     }
+
+
+    // FOR SOME REASON YOUTUBE DOESNT WANT TO ACCEPT MY DECYPHERED SIGNATURE
+    // 403 FORBIDDEN ???
+    //
+    ///// <summary>
+    ///// Parses a player for decyphering signatures
+    ///// </summary>
+    ///// <param name="cancellationToken">The token to cancel this action</param>
+    ///// <returns>The player signature timestamp and operations list</returns>
+    ///// <exception cref="Exception"></exception>
+    //public async Task<(string signatureTimestamp, List<Func<string, string>> operations)> ParsePlayerAsync(
+    //    CancellationToken cancellationToken = default)
+    //{
+    //    // iframe
+    //    string iframeUrl = Endpoints.YouTubeWebUrl + Endpoints.Iframe;
+    //    string iframe = await requestHelper.GetAndValidateAsync(iframeUrl, null, cancellationToken);
+
+    //    // Player
+    //    string playerVersion = Regex.Match(iframe, @"player\\?/([0-9a-fA-F]{8})\\?/").Groups[1].Value;
+    //    string playerSourceUrl = Endpoints.YouTubeWebUrl + Endpoints.PlayerSource(playerVersion);
+    //    string playerSource = await requestHelper.GetAndValidateAsync(playerSourceUrl, null, cancellationToken);
+
+    //    // Signature
+    //    string signatureTimestamp = Regex.Match(playerSource, @"(?:signatureTimestamp|sts):(\d{5})").Groups[1].Value;
+
+    //    if (string.IsNullOrWhiteSpace(signatureTimestamp))
+    //        throw new Exception("Failed to find signature timestamp in player source");
+
+    //    // Cipher
+    //    string cipherCallsite = Regex.Match(playerSource,
+    //        """
+    //        [$_\w]+=function\([$_\w]+\){([$_\w]+)=\1\.split\(['"]{2}\);.*?return \1\.join\(['"]{2}\)}
+    //        """,
+    //                RegexOptions.Singleline).Groups[0].Value;
+    //    if (string.IsNullOrWhiteSpace(cipherCallsite))
+    //        throw new Exception("Failed to find cipher callsite in player source");
+
+    //    string cipherContainerName = Regex.Match(cipherCallsite, @"([$_\w]+)\.[$_\w]+\([$_\w]+,\d+\);").Groups[1].Value;
+    //    if (string.IsNullOrWhiteSpace(cipherContainerName))
+    //        throw new Exception("Failed to find cipher container name in player source");
+
+    //    string cipherDefinition = Regex.Match(playerSource,
+    //        $$"""
+    //        var {{Regex.Escape(cipherContainerName)}}={.*?};
+    //        """,
+    //        RegexOptions.Singleline).Groups[0].Value;
+    //    if (string.IsNullOrWhiteSpace(cipherDefinition))
+    //        throw new Exception("Failed to find cipher definition in player source");
+
+    //    // Functions
+    //    string swapFuncName = Regex.Match(cipherDefinition, @"([$_\w]+):function\([$_\w]+,[$_\w]+\){+[^}]*?%[^}]*?}", RegexOptions.Singleline).Groups[1].Value;
+    //    string spliceFuncName = Regex.Match(cipherDefinition, @"([$_\w]+):function\([$_\w]+,[$_\w]+\){+[^}]*?splice[^}]*?}", RegexOptions.Singleline).Groups[1].Value;
+    //    string reverseFuncName = Regex.Match(cipherDefinition, @"([$_\w]+):function\([$_\w]+\){+[^}]*?reverse[^}]*?}", RegexOptions.Singleline).Groups[1].Value;
+
+    //    // Operations
+    //    List<Func<string, string>> operations = new();
+    //    foreach (string statement in cipherCallsite.Split(';'))
+    //    {
+    //        string calledFuncName = Regex.Match(statement, @"[$_\w]+\.([$_\w]+)\([$_\w]+,\d+\)").Groups[1].Value;
+    //        if (string.IsNullOrWhiteSpace(calledFuncName))
+    //            continue;
+
+    //        if (string.Equals(calledFuncName, swapFuncName, StringComparison.Ordinal))
+    //        {
+    //            int index = int.Parse(Regex.Match(statement, @"\([$_\w]+,(\d+)\)").Groups[1].Value);
+    //            operations.Add(s => new StringBuilder(s)
+    //                {
+    //                    [0] = s[index],
+    //                    [index] = s[0],
+    //                }.ToString());
+    //        }
+    //        else if (string.Equals(calledFuncName, spliceFuncName, StringComparison.Ordinal))
+    //        {
+    //            int index = int.Parse(Regex.Match(statement, @"\([$_\w]+,(\d+)\)").Groups[1].Value);
+    //            operations.Add(s => s.Substring(index));
+    //        }
+    //        else if (string.Equals(calledFuncName, reverseFuncName, StringComparison.Ordinal))
+    //        {
+    //            operations.Add(s =>
+    //            {
+    //                s.Reverse();
+    //                return s;
+    //            });
+    //        }
+    //    }
+
+    //    return (signatureTimestamp, operations);
+    //}
 }
--- YouTubeMusicAPI/Internal/Endpoints.cs
+++ YouTubeMusicAPI/Internal/Endpoints.cs
@@ -8,18 +8,23 @@ internal abstract class Endpoints
     /// <summary>
     /// The base url to the YouTube Music internal API
     /// </summary>
-    public const string BaseUrl = "https://music.youtube.com/youtubei/v1";
+    public const string MusicApiUrl = "https://music.youtube.com/youtubei/v1";
 
     /// <summary>
     /// The web url to YouTube Music
     /// </summary>
-    public const string WebUrl = "https://music.youtube.com";
+    public const string MusicWebUrl = "https://music.youtube.com";
+    
+    /// <summary>
+    /// The web url to YouTube
+    /// </summary>
+    public const string YouTubeWebUrl = "https://www.youtube.com";
 
     /// <summary>
     /// The url to consent to Googles cookies
     /// </summary>
-    public const string Cookies = "https://consent.youtube.com";
-
+    public const string CookiesUrl = "https://consent.youtube.com";
+    
 
     /// <summary>
     /// The path the search endpoint
@@ -50,4 +55,17 @@ internal abstract class Endpoints
     /// The path the save Googles cookies
     /// </summary>
     public const string Save = "/save";
+
+    ///// <summary>
+    ///// The path the iframe api
+    ///// </summary>
+    //public const string Iframe = "/iframe_api";
+
+
+    ///// <summary>
+    ///// The path to the player source for a specific version
+    ///// </summary>
+    //public static string PlayerSource(
+    //    string version) =>
+    //    $"/s/player/{version}/player_ias.vflset/en_US/base.js";
 }

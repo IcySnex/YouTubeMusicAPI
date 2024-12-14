138e17f8144e5fd972f5138f0deef9528492ca85
IcySnex <lao43919@gmail.com>
2024-12-14 22:54:38 +0100

    Created models for SongVideo streams

---
+++ YouTubeMusicAPI/Models/Streams/AudioStream.cs
@@ -0,0 +1,62 @@
+﻿namespace YouTubeMusicAPI.Models.Streams;
+
+/// <summary>
+/// Represents a YouTube Music audio stream
+/// </summary>
+public class AudioStream : MediaStream
+{
+    /// <summary>
+    /// Creates a new AudioStream
+    /// </summary>
+    /// <param name="itag">The encoding identifier for the media stream</param>
+    /// <param name="url">The URL to access the media stream</param>
+    /// <param name="container">The container details for the media stream</param>
+    /// <param name="lastModifedAt">The last modified date of the media stream</param>
+    /// <param name="approximatedDuration">The approximated duration of the stream</param>
+    /// <param name="contentLenght">The content length of the media stream in bytes</param>
+    /// <param name="bitrate">The bitrate of the stream</param>
+    /// <param name="averageBitrate">The average bitrate of the stream</param>
+    /// <param name="quality">The quality of the audio stream</param>
+    /// <param name="sampleRate">The sample rate of the audio stream</param>
+    /// <param name="channels">The number of audio channels</param>
+    /// <param name="loudnessDb">The loudness of the audio stream in decibels</param>
+    public AudioStream(
+        int itag,
+        string url,
+        MediaContainer container,
+        DateTime lastModifedAt,
+        TimeSpan approximatedDuration,
+        long contentLenght,
+        long bitrate,
+        long averageBitrate,
+        string quality,
+        int sampleRate,
+        int channels,
+        double loudnessDb) : base(itag, url, container, lastModifedAt, approximatedDuration, contentLenght, bitrate, averageBitrate)
+    {
+        Quality = quality;
+        SampleRate = sampleRate;
+        Channels = channels;
+        LoudnessDb = loudnessDb;
+    }
+
+    /// <summary>
+    /// The quality of the audio stream
+    /// </summary>
+    public string Quality { get; }
+
+    /// <summary>
+    /// The sample rate of the audio stream
+    /// </summary>
+    public int SampleRate { get; }
+
+    /// <summary>
+    /// The number of audio channels
+    /// </summary>
+    public int Channels { get; }
+
+    /// <summary>
+    /// The loudness of the audio stream in decibels
+    /// </summary>
+    public double LoudnessDb { get; }
+}
---
+++ YouTubeMusicAPI/Models/Streams/MediaStream.cs
@@ -0,0 +1,78 @@
+﻿namespace YouTubeMusicAPI.Models.Streams;
+
+/// <summary>
+/// Represents a YouTube Music media stream
+/// </summary>
+public abstract class MediaStream
+{
+    /// <summary>
+    /// Creates a new MediaStream
+    /// </summary>
+    /// <param name="itag">The encoding identifier for the media stream</param>
+    /// <param name="url">The URL to access the media stream</param>
+    /// <param name="container">The container details for the media stream</param>
+    /// <param name="lastModifedAt">The last modified date of the media stream</param>
+    /// <param name="approximatedDuration">The approximated duration of the stream</param>
+    /// <param name="contentLenght">The content length of the media stream in bytes</param>
+    /// <param name="bitrate">The bitrate of the stream</param>
+    /// <param name="averageBitrate">The average bitrate of the stream</param>
+    protected MediaStream(
+        int itag,
+        string url,
+        MediaContainer container,
+        DateTime lastModifedAt,
+        TimeSpan approximatedDuration,
+        long contentLenght,
+        long bitrate,
+        long averageBitrate)
+    {
+        Itag = itag;
+        Url = url;
+        Container = container;
+        LastModifedAt = lastModifedAt;
+        ApproximatedDuration = approximatedDuration;
+        ContentLenght = contentLenght;
+        Bitrate = bitrate;
+        AverageBitrate = averageBitrate;
+    }
+
+    /// <summary>
+    /// The encoding identifier for the media stream
+    /// </summary>
+    public int Itag { get; }
+
+    /// <summary>
+    /// The URL to access the media stream
+    /// </summary>
+    public string Url { get; }
+
+    /// <summary>
+    /// The container details for the media stream
+    /// </summary>
+    public MediaContainer Container { get; }
+
+    /// <summary>
+    /// The last modified date of the media stream
+    /// </summary>
+    public DateTime LastModifedAt { get; }
+
+    /// <summary>
+    /// The approximated duration of the stream
+    /// </summary>
+    public TimeSpan ApproximatedDuration { get; }
+
+    /// <summary>
+    /// The content length of the media stream in bytes
+    /// </summary>
+    public long ContentLenght { get; }
+
+    /// <summary>
+    /// The bitrate of the stream
+    /// </summary>
+    public long Bitrate { get; }
+
+    /// <summary>
+    /// The average bitrate of the stream
+    /// </summary>
+    public long AverageBitrate { get; }
+}

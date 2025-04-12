e1c90ec6f4ae9ea26f5781d0d0b4c9e153a94b97
IcySnex <lao43919@gmail.com>
2025-04-12 15:30:07 +0200

    Introduced pagination system

---
+++ YouTubeMusicAPI/Common/FetchPageDelegate.cs
@@ -0,0 +1,12 @@
+﻿namespace YouTubeMusicAPI.Common;
+
+/// <summary>
+/// Delegate for fetching a page of results using an optional continuation token
+/// </summary>
+/// <typeparam name="T">The type of items</typeparam>
+/// <param name="continuationToken">The token to fetch the next page (null for the first page)</param>
+/// <param name="cancellationToken">The token to cancel this action</param>
+/// <returns>The page corresponding to the continuation token</returns>
+public delegate Task<Page<T>> FetchPageDelegate<T>(
+    string? continuationToken,
+    CancellationToken cancellationToken = default);

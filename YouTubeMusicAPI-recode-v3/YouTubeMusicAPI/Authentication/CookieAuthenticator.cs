5da7db467576e69835003c4df2f64600beee0d63
IcySnex <lao43919@gmail.com>
2025-05-02 22:48:23 +0200

    Added new project for recode (Version 3)

---
+++ YouTubeMusicAPI-recode-v3/YouTubeMusicAPI/Authentication/CookieAuthenticator.cs
@@ -0,0 +1,79 @@
+﻿using System.Globalization;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using YouTubeMusicAPI.Utils;
+
+namespace YouTubeMusicAPI.Authentication;
+
+/// <summary>
+/// Represents an authenticated user session via cookies used to authenticate HTTP requests sent to YouTube Music.
+/// </summary>
+public class CookieAuthenticator : AnonymousAuthenticator, IAuthenticator
+{
+    /// <summary>
+    /// The container used to store cookies for authentication.
+    /// </summary>
+    public CookieContainer Container { get; } = new();
+
+    /// <summary>
+    /// Creates a new instance of the <see cref="CookieAuthenticator"/> class.
+    /// </summary>
+    /// <param name="cookies">The cookies used to authenticate the user session.</param>
+    /// <param name="visitorData">Represents a unique identifier used to authenticate and link YouTube requests to a user. Leave this <see langword="null"/> to use randomly generated visitor data.</param>
+    /// <param name="prooOfOriginToken">Represents a unique security token used to verify the authenticity of a client for YouTube requests. May be required when fetching streaming data.</param>
+    /// <param name="apiKey">Represents the API key used to validate the YouTube client.</param>
+    /// <param name="userAgent">Represents the user agent sent with the request to identify the client making the YouTube request.</param>
+    public CookieAuthenticator(
+        IEnumerable<Cookie> cookies,
+        string? visitorData = null,
+        string? prooOfOriginToken = null,
+        string apiKey = DefaultApiKey,
+        string userAgent = DefaultUserAgent) : base(visitorData, prooOfOriginToken, apiKey, userAgent)
+    {
+        foreach (Cookie cookie in cookies)
+            Container.Add(cookie);
+
+        Container.Add(new Cookie("SOCS", "CAI") { Domain = ".youtube.com" });
+    }
+
+
+    string GenerateAuthHeaderValue(
+        Uri uri)
+    {
+        IReadOnlyCollection<Cookie> cookies = Container.GetCookies(uri);
+
+        string? sessionId = (cookies.FirstOrDefault(c => string.Equals(c.Name, "__Secure-3PAPISID", StringComparison.Ordinal)) ?? cookies.FirstOrDefault(c => string.Equals(c.Name, "SAPISID", StringComparison.Ordinal)))?.Value;
+        Ensure.NotNullOrEmpty(sessionId, nameof(sessionId));
+
+        long timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+        string domain = uri.Scheme + Uri.SchemeDelimiter + uri.Host;
+
+        string token = $"{timestamp} {sessionId} {domain}";
+        byte[] tokenHashData = SHA1.HashData(Encoding.UTF8.GetBytes(token));
+
+        StringBuilder tokenHashBuffer = new(2 * tokenHashData.Length);
+        foreach (byte value in tokenHashData)
+            tokenHashBuffer.Append(value.ToString("X2", CultureInfo.InvariantCulture));
+
+        return $"SAPISIDHASH {timestamp}_{tokenHashBuffer}";
+    }
+
+
+    /// <summary>
+    /// Applies the authentication to the given HTTP request.
+    /// </summary>
+    /// <param name="request">The HTTP request to which the authentication will be applied.</param>
+    /// <exception cref="ArgumentNullException">Occurrs when the RequestUri of the request is <see langword="null"/>./></exception>
+    public override void Apply(
+        HttpRequestMessage request)
+    {
+        base.Apply(request);
+
+        Ensure.NotNull(request.RequestUri, nameof(request.RequestUri));
+
+        request.Headers.Remove("Cookie");
+        request.Headers.Add("Cookie", Container.GetCookieHeader(request.RequestUri));
+        request.Headers.Add("Authorization", GenerateAuthHeaderValue(request.RequestUri));
+    }
+}
---
+++ YouTubeMusicAPI-recode-v3/YouTubeMusicApi.Tests/Utils/UrlTests.cs
@@ -0,0 +1,29 @@
+﻿using YouTubeMusicAPI.Utils;
+
+namespace YouTubeMusicApi.Tests.Utils;
+
+[TestFixture]
+public class UrlTests
+{
+    [Test]
+    [TestCase("https://api.example.com/endpoint", "key1", ExpectedResult = "https://api.example.com/endpoint")]
+    [TestCase("https://api.example.com/endpoint?key1=value1", "key1", ExpectedResult = "https://api.example.com/endpoint")]
+    [TestCase("https://api.example.com/endpoint?key1=value1&key2=value2", "key1", ExpectedResult = "https://api.example.com/endpoint?key2=value2")]
+    [TestCase("https://api.example.com/endpoint?key1=value1&key2=value2", "key2", ExpectedResult = "https://api.example.com/endpoint?key1=value1")]
+    [TestCase("https://api.example.com/endpoint?key1=value1&key2=value2&key3=value3", "key2", ExpectedResult = "https://api.example.com/endpoint?key1=value1&key3=value3")]
+    public string Should_remove_parameter_from_query(
+        string url,
+        string key) =>
+        Url.RemoveQueryParameter(url, key);
+
+    [Test]
+    [TestCase("https://api.example.com/endpoint", "key1", "value1", ExpectedResult = "https://api.example.com/endpoint?key1=value1")]
+    [TestCase("https://api.example.com/endpoint?key1=value1", "key1", "newValue1", ExpectedResult = "https://api.example.com/endpoint?key1=newValue1")]
+    [TestCase("https://api.example.com/endpoint?key1=value1&key2=value2", "key1", "newValue1", ExpectedResult = "https://api.example.com/endpoint?key2=value2&key1=newValue1")]
+    [TestCase("https://api.example.com/endpoint?key1=value1&key2=value2", "key2", "newValue2", ExpectedResult = "https://api.example.com/endpoint?key1=value1&key2=newValue2")]
+    public string Should_add_or_replace_parameter_from_query(
+        string url,
+        string key,
+        string value) =>
+        Url.SetQueryParameter(url, key, value);
+}

aafb5c40cb35247eb1a102b8aceef12998b30a83
IcySnex <lao43919@gmail.com>
2025-05-03 20:51:14 +0200

    Introduced ability to generate visitor data

---
+++ YouTubeMusicAPI-recode-v3/YouTubeMusicAPI/Authentication/IdentityGenerator.cs
@@ -0,0 +1,54 @@
+﻿using System.Text.RegularExpressions;
+using YouTubeMusicAPI.Http;
+
+namespace YouTubeMusicAPI.Authentication;
+
+/// <summary>
+/// Contains static methods used to generate valid authentication data for YouTube Music.
+/// </summary>
+public static partial class IdentityGenerator
+{
+    [GeneratedRegex("\"visitorData\":\"([^\"]+)")]
+    private static partial Regex VisitorDataRegex();
+
+
+    /// <summary>
+    /// Generates valid visitor data used to authenticate YouTube requests.
+    /// </summary>
+    /// <param name="cancellationToken">The token to cancel this task.</param>
+    /// <returns>A string representing the visitor data.</returns>
+    /// <exception cref="InvalidDataException">Occurs when the visitor data could not be extracted from the HTML content.</exception>"
+    /// <exception cref="HttpRequestException">Occurs when the HTTP request fails.</exception>"
+    /// <exception cref="OperationCanceledException">Occurs when this task was cancelled.</exception>
+    public static async Task<string> VisitorDataAsync(
+        CancellationToken cancellationToken = default)
+    {
+        using HttpClient client = new();
+
+        string url = Endpoints.YouTubeUrl + Endpoints.Embed("um0ETkJABmI");
+        HttpRequestMessage request = new(HttpMethod.Get, url);
+
+        HttpResponseMessage respone = await client.SendAsync(request, cancellationToken).ConfigureAwait(false);
+        respone.EnsureSuccessStatusCode();
+
+        string htmlContent = await respone.Content.ReadAsStringAsync(cancellationToken).ConfigureAwait(false);
+
+        Match match = VisitorDataRegex().Match(htmlContent);
+        if (!match.Success)
+            throw new InvalidDataException("Visitor data could not be extracted from the HTML content: Found no Regex match.");
+
+        return match.Groups[1].Value;
+    }
+
+    /// <summary>
+    /// Generates a valid proof of origin token used to verify the authenticity of the client for YouTube requests.
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns>A string representing the proof of origin token.</returns>
+    [Obsolete("Currently not supported. Please check https://github.com/yt-dlp/yt-dlp/wiki/PO-Token-Guide to manually fetch a proof of origin token.", true)]
+    public static Task<string> ProofOfOriginTokenAsync(
+        CancellationToken cancellationToken = default)
+    {
+        return Task.FromResult(string.Empty);
+    }
+}

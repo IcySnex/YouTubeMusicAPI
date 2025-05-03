384bd9a7d8963012d5bf2071014b6513cc76fb28
IcySnex <lao43919@gmail.com>
2025-05-03 20:51:14 +0200

    Introduced ability to generate visitor data

---
+++ YouTubeMusicAPI-recode-v3/YouTubeMusicApi.Tests/Authentication/IdentityGeneratorTests.cs
@@ -0,0 +1,44 @@
+﻿using YouTubeMusicAPI.Authentication;
+
+namespace YouTubeMusicApi.Tests.Authentication;
+
+[TestFixture]
+public class IdentityGeneratorTests
+{
+    [Test]
+    public void Should_generate_visitor_data()
+    {
+        // Act
+        string? visitorData = null;
+
+        Assert.DoesNotThrowAsync(async () =>
+        {
+            visitorData = await IdentityGenerator.VisitorDataAsync();
+        });
+
+        // Assert
+        Assert.That(visitorData, Is.Not.Null.Or.Empty);
+
+        // Output
+        TestContext.Out.WriteLine("VisitorData: {0}", visitorData);
+    }
+
+    [Test]
+    [Ignore("Currently not supported.")]
+    public void Should_generate_proof_of_origin_token()
+    {
+        // Act
+        string? proofOfOriginToken = null;
+
+        //Assert.DoesNotThrowAsync(async () =>
+        //{
+        //    proofOfOriginToken = await IdentityGenerator.ProofOfOriginTokenAsync();
+        //});
+
+        // Assert
+        Assert.That(proofOfOriginToken, Is.Not.Null.Or.Empty);
+
+        // Output
+        TestContext.Out.WriteLine("Proof of Origin Token: {0}", proofOfOriginToken);
+    }
+}

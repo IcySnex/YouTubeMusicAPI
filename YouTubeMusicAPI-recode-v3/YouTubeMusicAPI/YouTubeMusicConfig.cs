0de1c3cc5832d09cd08cbe58db708e5ac45da0f7
IcySnex <lao43919@gmail.com>
2025-05-10 20:04:12 +0200

    Implemented YouTubeMusicConfig

---
+++ YouTubeMusicAPI-recode-v3/YouTubeMusicAPI/YouTubeMusicConfig.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Logging;
+using YouTubeMusicAPI.Authentication;
+
+namespace YouTubeMusicAPI;
+
+/// <summary>
+/// Contains configuration options for the YouTube Music client.
+/// </summary>
+public class YouTubeMusicConfig
+{
+    /// <summary>
+    /// The geographical location for YouTube Music requests.
+    /// </summary>
+    public string GeographicalLocation { get; init; } = "US";
+
+    /// <summary>
+    /// The logger instance used to provide progress and error messages.
+    /// </summary>
+    public ILogger? Logger { get; init; }
+
+    IAuthenticator? authenticator = null;
+    /// <summary>
+    /// The authenticator used to authenticate HTTP requests sent to YouTube Music.
+    /// </summary>
+    public IAuthenticator Authenticator
+    {
+        get => authenticator ??= new AnonymousAuthenticator();
+        init => authenticator = value;
+    }
+
+    HttpClient? httpClient = null;
+    /// <summary>
+    /// The HTTP client used to send requests to YouTube Music.
+    /// </summary>
+    public HttpClient HttpClient
+    {
+        get => httpClient ??= new();
+        init => httpClient = value;
+    }
+}

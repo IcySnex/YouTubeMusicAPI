936082db07f045d33c4fd3ba5f22513c3b2d18e5
IcySnex <81762750+IcySnex@users.noreply.github.com>
2025-07-30 14:10:38 +0200

    Implemented ability to get information about the currently authenticated user

---
+++ YouTubeMusicAPI/Models/AuthenticatedUser.cs
@@ -0,0 +1,91 @@
+﻿using System.Text.Json;
+using YouTubeMusicAPI.Utils;
+
+namespace YouTubeMusicAPI.Models;
+
+/// <summary>
+/// Represents an authenticated user on YouTube Music.
+/// </summary>
+/// <remarks>
+/// Creates a new instance of <see cref="AuthenticatedUser"/>.
+/// </remarks>
+/// <param name="name">The name of this authenticated user.</param>
+/// <param name="id">The ID of this authenticated user.</param>
+/// <param name="thumbnails">The thumbnails of this authenticated user.</param>
+/// <param name="handle">The handle of this authenticated user.</param>
+/// <param name="isPremium">Whether this authenticated user is subscribed to premium.</param>
+public class AuthenticatedUser(
+    string name,
+    string id,
+    Thumbnail[] thumbnails,
+    string handle,
+    bool isPremium) : YouTubeMusicEntity(name, id, id)
+{
+    /// <summary>
+    /// Parses the JSON element into a <see cref="AuthenticatedUser"/>.
+    /// </summary>
+    /// <param name="element">The JSON item "multiPageMenuRenderer".</param>
+    internal static AuthenticatedUser Parse(
+        JsonElement element)
+    {
+        JsonElement headerRenderer = element
+            .GetProperty("header")
+            .GetProperty("activeAccountHeaderRenderer");
+        
+        JsonElement items = element
+            .GetProperty("sections")
+            .GetElementAt(0)
+            .GetProperty("multiPageMenuSectionRenderer")
+            .GetProperty("items");
+
+
+        string name = headerRenderer
+            .SelectRunTextAt("accountName");
+
+        string id = items
+            .GetElementAt(0)
+            .GetProperty("compactLinkRenderer")
+            .SelectNavigationBrowseId();
+
+        Thumbnail[] thumbnails = headerRenderer
+            .SelectThumbnails("accountPhoto");
+
+        string handle = headerRenderer
+            .SelectRunTextAt("channelHandle");
+
+        bool isPremium = items
+            .GetElementAt(1)
+            .GetProperty("compactLinkRenderer")
+            .SelectRunTextAt("title")
+            .If("Paid Memberships", true, false);
+
+        return new(name, id, thumbnails, handle, isPremium);
+    }
+
+
+    /// <summary>
+    /// The ID of this authenticated user.
+    /// </summary>
+    public override string Id { get; } = id;
+
+    /// <summary>
+    /// The browse ID of this authenticated user.
+    /// </summary>
+    public override string BrowseId { get; } = id;
+
+
+    /// <summary>
+    /// The thumbnails of this authenticated user.
+    /// </summary>
+    public Thumbnail[] Thumbnails { get; } = thumbnails;
+
+    /// <summary>
+    /// The handle of this authenticated user.
+    /// </summary>
+    public string Handle { get; } = handle;
+
+    /// <summary>
+    /// Whether this authenticated user is subscribed to premium.
+    /// </summary>
+    public bool IsPremium { get; } = isPremium;
+}
--- YouTubeMusicAPI/YouTubeMusicClient.cs
+++ YouTubeMusicAPI/YouTubeMusicClient.cs
@@ -1,5 +1,9 @@
-﻿using YouTubeMusicAPI.Http;
+﻿using Microsoft.Extensions.Logging;
+using System.Text.Json;
+using YouTubeMusicAPI.Http;
+using YouTubeMusicAPI.Models;
 using YouTubeMusicAPI.Services;
+using YouTubeMusicAPI.Utils;
 
 namespace YouTubeMusicAPI;
 
@@ -8,6 +12,8 @@ namespace YouTubeMusicAPI;
 /// </summary>
 public class YouTubeMusicClient
 {
+    RequestHandler requestHandler;
+
     /// <summary>
     /// Creates a new instance of the <see cref="YouTubeMusicClient"/> class.
     /// </summary>
@@ -17,7 +23,7 @@ public class YouTubeMusicClient
     {
         Config = config ?? new();
 
-        RequestHandler requestHandler = new(Config.GeographicalLocation, Config.Authenticator, Config.HttpClient, Config.Logger);
+        requestHandler = new(Config.GeographicalLocation, Config.Authenticator, Config.HttpClient, Config.Logger);
         Search = new(requestHandler, Config.Logger);
         Info = new(requestHandler, Config.Logger);
     }
@@ -38,4 +44,43 @@ public class YouTubeMusicClient
     /// The service used to get information from YouTube Music.
     /// </summary>
     public InfoService Info { get; }
+
+
+    /// <summary>
+    /// Gets the currently authenticated user on YouTube Music.
+    /// </summary>
+    /// <param name="cancellationToken">The token to cancel this action.</param>
+    /// <returns>An <see cref="AuthenticatedUser"/> if the user is authenticated; otherwise, <c>null</c>.</returns>
+    public async Task<AuthenticatedUser?> GetAuthenticatedUserAsync(
+        CancellationToken cancellationToken = default)
+    {
+        if (!requestHandler.IsAuthenticated)
+        {
+            Config.Logger?.LogInformation("[YouTubeMusicClient-GetAuthenticatedUserAsync] Could not get authenticated user: RequestHandler not authenticated.");
+            return null;
+        }
+
+        // Send request
+        string response = await requestHandler.PostAsync(Endpoints.AccountMenu, null, ClientType.WebMusic, cancellationToken);
+
+        // Parse response
+        using JsonDocument json = JsonDocument.Parse(response);
+        JsonElement rootElement = json.RootElement;
+
+        JsonElement menuRenderer = rootElement
+            .GetProperty("actions")
+            .GetElementAt(0)
+            .GetProperty("openPopupAction")
+            .GetProperty("popup")
+            .GetProperty("multiPageMenuRenderer");
+
+        if (!menuRenderer.TryGetProperty("header", out _))
+        {
+            Config.Logger?.LogInformation("[YouTubeMusicClient-GetAuthenticatedUserAsync] Could not get authenticated user: User not logged in.");
+            return null;
+        }
+
+        AuthenticatedUser user = AuthenticatedUser.Parse(menuRenderer);
+        return user;
+    }
 }

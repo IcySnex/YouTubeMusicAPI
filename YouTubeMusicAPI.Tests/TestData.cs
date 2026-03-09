a37902b69a19399932b13720f6b7b3a2b8af0a5a
IcySnex <81762750+IcySnex@users.noreply.github.com>
2026-03-09 19:22:34 +0100

    Updated TestData

---
+++ YouTubeMusicAPI.Tests/TestData.cs
@@ -0,0 +1,109 @@
+using System.Net;
+
+namespace YouTubeMusicAPI.Tests;
+
+/// <summary>
+/// Contains test data for tests
+/// </summary>
+internal abstract class TestData
+{
+    /// <summary>
+    /// Test geographical location for requests
+    /// </summary>
+    public const string GeographicalLocation = "US";
+
+    /// <summary>
+    /// Test visitor data for requests
+    /// </summary>
+    public const string? VisitorData = "";
+
+    /// <summary>
+    /// Test po token for requests
+    /// </summary>
+    public const string? PoToken = "";
+        
+    /// <summary>
+    /// Test cookies for authentication
+    /// </summary>
+    public static IEnumerable<Cookie>? Cookies
+    {
+        get
+        {
+            string? cookies = "";
+                
+            return cookies?
+                .Split(';')
+                .Select(cookieString =>
+                {
+                    string[] parts = cookieString.Split("=");
+                    return new Cookie(parts[0], parts[1]) { Domain = ".youtube.com" };
+                }) ?? null;
+        }
+    }
+
+    /// <summary>
+    /// Test Player Id for streaming
+    /// </summary>
+    public static string? PlayerId = "267b6435";
+    
+
+    /// <summary>
+    /// Test query for search requests
+    /// </summary>
+    public const string SearchQuery = "Pashanim";
+
+
+    /// <summary>
+    /// Test offset from which items should be returned for fetch requests
+    /// </summary>
+    public const int FetchOffset = 0;
+
+    /// <summary>
+    /// Test maximum items of items which should be returned for fetch requests
+    /// </summary>
+    public const int FetchLimit = 20;
+
+
+    /// <summary>
+    /// Test song or video id
+    /// </summary>
+    public const string SongVideoId = "MLKBxET5Lzg";
+
+    /// <summary>
+    /// Test album id
+    /// </summary>
+    public const string AlbumId = "OLAK5uy_muEnh0WPCqRdkgV3Qg24ttvmZTP1_RBTo";
+
+    /// <summary>
+    /// Test playlist id
+    /// </summary>
+    public const string PlaylistId = "PLuvXOFt0CoEbwWSQj5LmzPhIVKS0SvJ-1";
+
+
+    /// <summary>
+    /// Test album browse id
+    /// </summary>
+    public const string AlbumBrowseId = "MPREb_H2RWN4XY0Ny";
+
+    /// <summary>
+    /// Test playlist browse id
+    /// </summary>
+    public const string PlaylistBrowseId = "RDAMVMnP4Q-iszqb0";
+
+    /// <summary>
+    /// Test artist browse id
+    /// </summary>
+    public const string ArtistBrowseId = "UC5_H9CxsfukWbjYeYepADsw";
+
+
+    /// <summary>
+    /// Test watch time for update requests
+    /// </summary>
+    public static TimeSpan WatchTime = TimeSpan.FromMinutes(1.2);
+
+
+    /// <summary>
+    /// File path to download test media stream
+    /// </summary>
+    public static readonly string FilePath = @$"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\test.mp4";
+}

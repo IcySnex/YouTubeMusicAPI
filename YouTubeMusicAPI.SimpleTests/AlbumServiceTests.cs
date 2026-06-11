92198fc1a82c115f4d9f7d5428be826c98df396f
imgen <imgen@hotmail.com>
2026-06-11 18:33:22 +0800

    Add a SimpleTests project

---
+++ YouTubeMusicAPI.SimpleTests/AlbumServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using YouTubeMusicAPI.Services.Albums;
+
+namespace YouTubeMusicAPI.SimpleTests;
+
+public class AlbumServiceTests
+{
+    [Fact]
+    public async Task Should_get_albums_from_an_artist()
+    {
+        var artistService = new YouTubeMusicClient();
+        const string beatlesBrowseId = "MPADUC2XdaAVUannpujzv32jcouQ";
+        var artistAlbums = await artistService.Albums.GetAllByArtistAsync(beatlesBrowseId,
+            AlbumCategory.Albums,
+            AlbumSortingOrder.Default,
+            TestContext.Current.CancellationToken);
+
+        artistAlbums.Albums.Should().NotBeEmpty();
+    }
+}
